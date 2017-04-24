b9ab36d931cb66044f5b3dcc40315999b69330f1
prusche <prusche@gmail.com>
2017-04-24 02:25:42 -0700

    form-based workflow feature completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,11 +1,47 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper #adds article_parms to fix the problems with params; added via private in the other tutorial
 
-  def index
+  def index #main page with the list of all articles, see index.html.erb
     @articles = Article.all
   end
 
-  def show
+  def show #page for each individual article, see show.html.erb
     @article = Article.find(params[:id])
+  end
+
+  def new #page for creating a new article, see new.html.erb
+    @article = Article.new
+  end
+
+  def create #method which creates the new article, which then displays it
+    @article = Article.new(article_params)
+    @article.save
+
+    flash.notice = "Article '#{@article.title}' Created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy #deletes article
+    @article = Article.find(params[:id])
+    @article.delete
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
+    redirect_to articles_path
+  end
+
+  def edit #displays the page to edit the article, see edit.html.erb
+    @article = Article.find(params[:id])
+  end
+
+  def update #method to edit the article
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 
 end
--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles
+  root to: 'articles#index' #so that localhost:3000 will show the index page instead of the rails page
+  resources :articles #This line tells Rails that we have a resource named articles and the router should expect requests to follow the RESTful model of web interaction (REpresentational State Transfer).
 end
